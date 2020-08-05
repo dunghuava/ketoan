8afd508c8f0a8a62502969d8b83734ce9b6864a3
Tr.Thuan <33859797+truongthuan97@users.noreply.github.com>
2020-08-05 18:20:44 +0700

    fix show region trang chủ

--- sources/item_project_region.php
+++ sources/item_project_region.php
@@ -1,14 +1,14 @@
 
 <?php foreach ($list_district as $key => $district) {
-	$sql1= "select * from #_district where district_id = {$district}";
-	$info_district = $d->o_fet($sql1);
+	$sql= "select * from #_district where district_id = {$district[0]['id_district']}";
+	$info_district = $d->o_fet($sql);
 	if ($key<4) {
 ?>
 	<div class="col-md-3">
 		<div class="item-area">
 			<a href="" title="">
 				<div class="img_area">
-					<img class="img_error" src="<?=URLPATH ?>thumb.php?src=<?=URLPATH ?>img_data/images/<?=$value['hinh_anh']?>&w=600&h=420&zc=0" alt="">
+					<img class="img_error" src="<?=URLPATH ?>thumb.php?src=<?=URLPATH ?>img_data/images/<?=$district['hinh_anh']?>&w=600&h=420&zc=0" alt="">
 				</div>
 				<div class="content_area">
 					<h3 class="title_area"><?=@$info_district[0]['district_name']?></h3>
